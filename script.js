96522095993816e1e633e18a53ea2e337b0a1b2a
futarino-roy <futarino.m+roy@gamil.com>
2024-05-07 13:46:59 +0900

    change picture

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ const ctx = canvas.getContext('2d');
 const parts = {
   face: ['image/face/1.png'],
   hair: ['image/hair/1.png'],
-  eyes: ['image/me/1.png'],
-  nose: ['image/hana/1.png'],
+  eyes: ['image/me/1.png','image/me/2.png'],
+  nose: ['image/hana/2.png'],
   mouth: ['image/mouth/1.png']
 };
 
@@ -30,11 +30,11 @@ let lastEditedPart = '';
 // 顔を描画する関数
 function drawFace() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawPart(currentParts.face, 25, 30, 200, 200);
-  drawPart(currentParts.hair, 25, 30, 200, 150);
-  drawPart(currentParts.eyes, 102, 110, 40, 40);
-  drawPart(currentParts.nose, 112, 150, 20, 20);
-  drawPart(currentParts.mouth, 98, 190, 50, 30);
+  drawPart(currentParts.face, 0, 0, 250, 250);
+  drawPart(currentParts.hair, 0, 0, 250, 250);
+  drawPart(currentParts.eyes, 105, 130, 40, 18);
+  drawPart(currentParts.nose, 0, 0, 250, 250);
+  drawPart(currentParts.mouth, 0, 0, 250, 250);
 }
 
 // パーツを描画する関数
@@ -73,13 +73,29 @@ function changePart(partType, partSrc) {
 }
 
 // ページロード時にパーツリストを非表示にする
+// window.onload = function() {
+//   const lists = document.querySelectorAll('.parts-list');
+//   lists.forEach(list => {
+//     list.style.display = 'none';
+//   });
+//   drawFace(); // 顔を描画
+// };
+
 window.onload = function() {
+  // ページがロードされたときにローカルストレージからfaceの選択を読み取り
+  const storedFace = localStorage.getItem('selectedFace');
+  if (storedFace) {
+    // 選択されたfaceのパスがある場合は、それをcurrentPartsに設定
+    currentParts.face = storedFace;
+  }
   const lists = document.querySelectorAll('.parts-list');
   lists.forEach(list => {
     list.style.display = 'none';
   });
   drawFace(); // 顔を描画
 };
+
+
 
 // ダウンロードボタンをクリックしたときの処理
 // PNG形式で顔をダウンロードする関数
